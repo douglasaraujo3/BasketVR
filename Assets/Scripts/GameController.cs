5386474d827021237952cb60f3f76db749d97860
Douglas Araujo <douglas@duxinnovation.com>
2018-03-12 17:28:42 -0300

    Project commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public PlayerScript player;
+	public float timer = 5.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.holdingBall == false) {
+			timer -= Time.deltaTime;
+			if (timer <= 0) {
+				//Instantiate (ballPrefab);
+				SceneManager.LoadScene ("GameScene");
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public BallScript ball;
+	public GameObject playerGamera;
+	public float ballDistance;
+	public float ballForce;
+
+
+
+	public bool holdingBall = true;
+
+	// Use this for initialization
+	void Start () {
+		ball.GetComponent<Rigidbody> ().useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (holdingBall) {
+			ball.transform.position = playerGamera.transform.position + playerGamera.transform.forward * ballDistance;
+			if(Input.GetMouseButtonDown(0)){
+				holdingBall = false;
+				ball.ActivateTrail ();
+				ball.GetComponent<Rigidbody> ().useGravity = true;
+				ball.GetComponent<Rigidbody>().AddForce (playerGamera.transform.forward * ballForce);
+			}
+		}
+	}
+	public void shootBall(){
+		holdingBall = false;
+		ball.ActivateTrail ();
+		ball.GetComponent<Rigidbody> ().useGravity = true;
+		ball.GetComponent<Rigidbody>().AddForce (playerGamera.transform.forward * ballForce);
+	}
+}
