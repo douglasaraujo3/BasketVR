7c9ed82a99e96d6a8f3cee27a7b0ec831ab5181e
Douglas Araujo <douglas@duxinnovation.com>
2018-04-28 12:26:47 -0300

    Pontuacao

--- Assets/Scripts/ScoreArea.cs
+++ Assets/Scripts/ScoreArea.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreArea : MonoBehaviour {
-	
+	public static int pontuacao = 0;
 	public GameObject effectObject;
+	public Text textoPontos;
 	void Start(){
 		effectObject.SetActive (false);
-
+		textoPontos.text = pontuacao.ToString ();
 	}
 
 	void OnTriggerEnter(Collider c){
 		if (c.GetComponent<BallScript> () != null) {
+			pontuacao++;
 			effectObject.SetActive (true);
+			textoPontos.text = pontuacao.ToString ();
 		}
 	}
 
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # BasketVR
+
+
+# Play Instructions:
+# 1 - Point the ball to the basket
+# 2 - Touch and throw de ball
+# 3 - Wait the ball shows again and continue playing
+
+
+# Created By: Douglas Araujo & Luiz Zabuscka
